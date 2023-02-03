b8d980491f820ee3f2eeffebefc81b4d9b2b8a0a
Smael Nicolas <nico.smael@gmail.com>
2023-02-03 13:07:57 -0300

    added dummy data

---
+++ src/constants/data.js
@@ -0,0 +1,56 @@
+export const PLACES = [
+	{
+		id: 1,
+		category: "playa",
+		title: "Mar del Plata",
+		location: "Provincia de Buenos Aires",
+		description:
+			"Es una ciudad balnearia argentina en la costa del Atlántico. Su larga franja de playas incluye la amplia Punta Mogotes y Playa Grande, con sus rompientes para el surf. Detrás de Playa Grande, las calles rodeadas de árboles del barrio Los Troncos tienen elegantes casas de comienzos del siglo XX, que ahora son museos",
+		img: "https://photo620x400.mnstatic.com/b58a47aa20f66d3a80b98a2edc74637a/costanera-.jpg",
+	},
+	{
+		id: 2,
+		category: "playa",
+		title: "Monte Hermoso",
+		location: "Provincia de Buenos Aires",
+		description:
+			"Es la ciudad cabecera del Partido-Municipio homónimo, ubicado en el interior de la Provincia de Buenos Aires, Argentina, en el esa provincia. Posee 6351 habitantes y es uno de los principales centros turísticos de la región.",
+		img: "https://www.telam.com.ar/thumbs/bluesteel/advf/imagenes/2022/04/626074b563332_1200.jpg",
+	},
+	{
+		id: 3,
+		category: "lago",
+		title: "Lago Nahuel Huapi",
+		location: "San Carlos de Bariloche, Villa La Angostura",
+		description:
+			"El lago Nahuel Huapi es un lago de origen glaciar de la Patagonia Argentina. Es compartido por las provincias argentinas del Neuquén (80%) y de Río Negro (20%), y rodeado mayormente por el parque nacional homónimo.Posee una superficie de 557 km² y se sitúa a una altitud de 764 m s. n. m. Se destaca por su profundidad (464 m de profundidad máxima) y sus ocho ramificaciones o brazos",
+		img: "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/27/89/8e.jpg",
+	},
+	{
+		id: 4,
+		category: "lago",
+		title: "Lago Argentino",
+		location: "El Calafate",
+		description:
+			"El Lago Argentino​ es el mayor de los grandes lagos patagónicos de la Argentina; y el más austral de su territorio continental, por detrás del fueguino lago Fagnano en la parte insular. El Lago Argentino se encuentra ubicado en el sector sudoeste de la provincia de Santa Cruz.",
+		img: "https://upload.wikimedia.org/wikipedia/commons/a/af/Lago_Argentino_-_panoramio.jpg",
+	},
+	{
+		id: 5,
+		category: "montaña",
+		title: "Aconcagua",
+		location: "Mendoza",
+		description:
+			"Integra la Cordillera Principal, la cual es un componente de la cordillera de los Andes. Con una altitud de 6960,8 m s. n. m. (metros sobre el nivel del mar),1​ es el pico más eminente de los hemisferios meridional y occidental, el más alto de la Tierra después del sistema de los Himalayas (Asia) y, por tanto, la cima más elevada en América.",
+		img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Aconcagua2016.jpg/640px-Aconcagua2016.jpg",
+	},
+	{
+		id: 6,
+		category: "montaña",
+		title: "Cerro Tronador",
+		location: " Argentina , Chile",
+		description:
+			"Es un volcán geológicamente inactivo; su origen está asociado al proceso de formación de los Andes en el periodo Pleistoceno, comenzando hace aproximadamente 1.3 millones de años y terminando hace 340000 años.Este cerro con el correr de los años ha sufrido una importante acción erosiva producto de los glaciares que se sitúan sobre él, por esto ha escarpado y reducido sus laderas hasta casi perder la forma de un volcán típico.",
+		img: "https://i0.wp.com/mochilaexpres.com/wp-content/uploads/2021/04/20191121_134146-scaled.jpg?fit=768%2C373&ssl=1",
+	},
+];
