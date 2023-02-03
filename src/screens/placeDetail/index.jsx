30431fb7a821556b6ca95f7ad1503359e8049fda
Smael Nicolas <nico.smael@gmail.com>
2023-02-03 17:39:31 -0300

    added component and styles to screen detail places

--- src/screens/placeDetail/index.jsx
+++ src/screens/placeDetail/index.jsx
@@ -1,5 +1,63 @@
-import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Image, Text, View } from "react-native";
+import { ActivityIndicatorCustom } from "../../components/activityIndicator";
+import { PLACES } from "../../constants/data";
+import { getWeather } from "../../utils/getWeather";
+import { styles } from "./styles";
 
-export const PlaceDetail = () => {
-	return <div>PlaceDetail</div>;
+export const PlaceDetail = ({ route }) => {
+	const { title } = route.params;
+	const [weather, setWeather] = useState(null);
+	const place = PLACES.find((product) => product.title === title);
+
+	const handleData = (data) => {
+		setTimeout(() => {
+			setWeather(data);
+		}, 2000);
+	};
+
+	useEffect(() => {
+		place.category === "playas" && getWeather(title, handleData);
+	}, []);
+
+	const renderWeather = () => {
+		if (place.category !== "playas") return;
+		return !weather ? (
+			<ActivityIndicatorCustom />
+		) : (
+			<View style={styles.containerWeather}>
+				<Text style={styles.weatherTitle}>Clima</Text>
+				<Text style={styles.descriptionWeather}>
+					Sensación Térmica : {weather.main.feels_like}{" "}
+				</Text>
+				<Text style={styles.descriptionWeather}>
+					Temperatura : {weather.main.temp}{" "}
+				</Text>
+				<Text style={styles.descriptionWeather}>
+					Temperatura Máxima : {weather.main.temp_max}{" "}
+				</Text>
+				<Text style={styles.descriptionWeather}>
+					Temperatura Mínima : {weather.main.temp_min}{" "}
+				</Text>
+				<Text style={styles.descriptionWeather}>
+					Cielo : {weather.weather[0].description}{" "}
+				</Text>
+			</View>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<LinearGradient
+				colors={["#97D0E3", "#9BCADA", "#EBD6EC"]}
+				start={{ x: 0.9, y: 0.05 }}
+				style={styles.container}>
+				<Image style={styles.image} source={{ uri: place.img }} />
+				<Text style={styles.description}>{place.description}</Text>
+				<Text style={styles.location}>🌍 {place.location}</Text>
+				{renderWeather()}
+			</LinearGradient>
+		</View>
+	);
 };
--- src/screens/placeDetail/styles.js
+++ src/screens/placeDetail/styles.js
@@ -1,3 +1,42 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	image: {
+		width: "100%",
+		height: 250,
+	},
+	description: {
+		paddingHorizontal: 20,
+		textAlign: "justify",
+		fontFamily: "MM-300",
+		alignSelf: "center",
+		marginTop: 20,
+	},
+	location: {
+		textAlign: "justify",
+		fontFamily: "MM-500",
+		alignSelf: "center",
+		marginTop: 20,
+	},
+	containerWeather: {
+		padding: 15,
+		marginVertical: 15,
+	},
+	weatherTitle: {
+		textAlign: "center",
+		marginVertical: 10,
+		fontSize: 25,
+		fontFamily: "MM-700",
+		color: "#7D4DB3",
+	},
+	descriptionWeather: {
+		fontFamily: "MM-600",
+		color: "#7D4DB3",
+		textTransform: "capitalize",
+		marginVertical: 3,
+		fontSize: 15,
+	},
+});
