01a39a7f4e65cc4e990e1135c0d4fcc5d9ef7652
Smael Nicolas <nico.smael@gmail.com>
2023-02-03 18:09:26 -0300

    added styles to activity indicator

--- src/components/activityIndicator/index.jsx
+++ src/components/activityIndicator/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { ActivityIndicator, View } from "react-native";
+import { styles } from "./styles";
 
 export const ActivityIndicatorCustom = () => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<ActivityIndicator size="large" color="#999999" />
 		</View>
 	);
